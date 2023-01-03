c73ab61a7a89b2b6a79cd010323245319e387205
yujin-kwak <yujin02371@gmail.com>
2023-01-03 18:33:24 +0900

    feat: 차트 재구현

--- app.js
+++ app.js
@@ -12,235 +12,294 @@ for(let i = 0; i < nav_a.length; i++) {
 }
 
 /************그래픽 차트 기능************/
-am5.ready(function() {
+const ctx = document.getElementById('UAENA_Chart');
 
-// Create root element
-// https://www.amcharts.com/docs/v5/getting-started/#Root_element
-const root = am5.Root.new("main_profile_chartdiv");
+const data = {
+  labels: ["1기", "2기", "3기", "4기", "5기"],
+  datasets: [{
+    barPercentage: 0.6,
+    base: 0,
+    label: '기수별 인원',
+    data: [10000, 30500, 40000, 50000, 43000],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)'
+    ],
+    hoverBackgroundColor:
+    ['rgba(255, 99, 132, 0.4)',
+      'rgba(255, 159, 64, 0.4)',
+      'rgba(255, 205, 86, 0.4)',
+      'rgba(75, 192, 192, 0.4)',
+      'rgba(54, 162, 235, 0.4)'
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)'
+    ],
+    borderRadius : 8,
+    borderWidth: 2
+  }]
+};
 
-// Set themes
-// https://www.amcharts.com/docs/v5/concepts/themes/
-root.setThemes([
-  am5themes_Animated.new(root)
-]);
-
-var data = [{
-  name: "1기",
-  steps: 10557,
-  pictureSettings: {
-    src: "./assets/images/1기_photocard.PNG"
+const config = {
+  type: 'bar',
+  data: data,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    },
+    plugins: {
+      legend: {
+        display: false
+      }
+    },
+    responsive :false
   }
-}, {
-  name: "2기",
-  steps: 18600,
-  pictureSettings: {
-    src: "./assets/images/2기_photocard.PNG"
-  }
-}, {
-  name: "3기",
-  steps: 30500,
-  pictureSettings: {
-    src: "./assets/images/3기_photocard.PNG"
-  }
-}, {
-  name: "4기",
-  steps: 50301,
-  pictureSettings: {
-    src: "./assets/images/4기_photocard.PNG"
-  }
-}, {
-  name: "5기",
-  steps: 45200,
-  pictureSettings: {
-    src: "./assets/images/5기_photocard.PNG"
-  }
-}];
+};
 
-// Create chart
-// https://www.amcharts.com/docs/v5/charts/xy-chart/
-var chart = root.container.children.push(
-  am5xy.XYChart.new(root, {
-    panX: false,
-    panY: false,
-    wheelX: "none",
-    wheelY: "none",
-    paddingBottom: 50,
-    paddingTop: 40
-  })
-);
-
-// Create axes
-// https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
-
-var xRenderer = am5xy.AxisRendererX.new(root, {});
-xRenderer.grid.template.set("visible", false);
-
-var xAxis = chart.xAxes.push(
-  am5xy.CategoryAxis.new(root, {
-    paddingTop:40,
-    categoryField: "name",
-    renderer: xRenderer
-  })
-);
+new Chart(ctx, config);
 
 
-var yRenderer = am5xy.AxisRendererY.new(root, {});
-yRenderer.grid.template.set("strokeDasharray", [3]);
 
-var yAxis = chart.yAxes.push(
-  am5xy.ValueAxis.new(root, {
-    min: 0,
-    renderer: yRenderer
-  })
-);
 
-// Add series
-// https://www.amcharts.com/docs/v5/charts/xy-chart/series/
-var series = chart.series.push(
-  am5xy.ColumnSeries.new(root, {
-    name: "Income",
-    xAxis: xAxis,
-    yAxis: yAxis,
-    valueYField: "steps",
-    categoryXField: "name",
-    sequencedInterpolation: true,
-    calculateAggregates: true,
-    maskBullets: false,
-    tooltip: am5.Tooltip.new(root, {
-      dy: -30,
-      pointerOrientation: "vertical",
-      labelText: "{valueY}"
-    })
-  })
-);
 
-series.columns.template.setAll({
-  strokeOpacity: 0,
-  cornerRadiusBR: 10,
-  cornerRadiusTR: 10,
-  cornerRadiusBL: 10,
-  cornerRadiusTL: 10,
-  maxWidth: 50,
-  fillOpacity: 0.8
-});
+// am5.ready(function() {
 
-var currentlyHovered;
+// // Create root element
+// // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+// const root = am5.Root.new("main_profile_chartdiv");
 
-series.columns.template.events.on("pointerover", function (e) {
-  handleHover(e.target.dataItem);
-});
+// // Set themes
+// // https://www.amcharts.com/docs/v5/concepts/themes/
+// root.setThemes([
+//   am5themes_Animated.new(root)
+// ]);
 
-series.columns.template.events.on("pointerout", function (e) {
-  handleOut();
-});
+// var data = [{
+//   name: "1기",
+//   steps: 10557,
+//   pictureSettings: {
+//     src: "./assets/images/1기_photocard.PNG"
+//   }
+// }, {
+//   name: "2기",
+//   steps: 18600,
+//   pictureSettings: {
+//     src: "./assets/images/2기_photocard.PNG"
+//   }
+// }, {
+//   name: "3기",
+//   steps: 30500,
+//   pictureSettings: {
+//     src: "./assets/images/3기_photocard.PNG"
+//   }
+// }, {
+//   name: "4기",
+//   steps: 50301,
+//   pictureSettings: {
+//     src: "./assets/images/4기_photocard.PNG"
+//   }
+// }, {
+//   name: "5기",
+//   steps: 45200,
+//   pictureSettings: {
+//     src: "./assets/images/5기_photocard.PNG"
+//   }
+// }];
 
-function handleHover(dataItem) {
-  if (dataItem && currentlyHovered != dataItem) {
-    handleOut();
-    currentlyHovered = dataItem;
-    var bullet = dataItem.bullets[0];
-    bullet.animate({
-      key: "locationY",
-      to: 1,
-      duration: 600,
-      easing: am5.ease.out(am5.ease.cubic)
-    });
-  }
-}
+// // Create chart
+// // https://www.amcharts.com/docs/v5/charts/xy-chart/
+// var chart = root.container.children.push(
+//   am5xy.XYChart.new(root, {
+//     panX: false,
+//     panY: false,
+//     wheelX: "none",
+//     wheelY: "none",
+//     paddingBottom: 50,
+//     paddingTop: 40
+//   })
+// );
 
-function handleOut() {
-  if (currentlyHovered) {
-    var bullet = currentlyHovered.bullets[0];
-    bullet.animate({
-      key: "locationY",
-      to: 0,
-      duration: 600,
-      easing: am5.ease.out(am5.ease.cubic)
-    });
-  }
-}
+// // Create axes
+// // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
 
-var circleTemplate = am5.Template.new({});
+// var xRenderer = am5xy.AxisRendererX.new(root, {});
+// xRenderer.grid.template.set("visible", false);
 
-series.bullets.push(function (root, series, dataItem) {
-  var bulletContainer = am5.Container.new(root, {});
-  var circle = bulletContainer.children.push(
-    am5.Circle.new(
-      root,
-      {
-        radius: 34
-      },
-      circleTemplate
-    )
-  );
+// var xAxis = chart.xAxes.push(
+//   am5xy.CategoryAxis.new(root, {
+//     paddingTop:40,
+//     categoryField: "name",
+//     renderer: xRenderer
+//   })
+// );
 
-  var maskCircle = bulletContainer.children.push(
-    am5.Circle.new(root, { radius: 27 })
-  );
 
-  // only containers can be masked, so we add image to another container
-  var imageContainer = bulletContainer.children.push(
-    am5.Container.new(root, {
-      mask: maskCircle
-    })
-  );
+// var yRenderer = am5xy.AxisRendererY.new(root, {});
+// yRenderer.grid.template.set("strokeDasharray", [3]);
 
-  var image = imageContainer.children.push(
-    am5.Picture.new(root, {
-      templateField: "pictureSettings",
-      centerX: am5.p50,
-      centerY: am5.p50,
-      width: 60,
-      height: 60
-    })
-  );
+// var yAxis = chart.yAxes.push(
+//   am5xy.ValueAxis.new(root, {
+//     min: 0,
+//     renderer: yRenderer
+//   })
+// );
 
-  return am5.Bullet.new(root, {
-    locationY: 0,
-    sprite: bulletContainer
-  });
-});
+// // Add series
+// // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+// var series = chart.series.push(
+//   am5xy.ColumnSeries.new(root, {
+//     name: "Income",
+//     xAxis: xAxis,
+//     yAxis: yAxis,
+//     valueYField: "steps",
+//     categoryXField: "name",
+//     sequencedInterpolation: true,
+//     calculateAggregates: true,
+//     maskBullets: false,
+//     tooltip: am5.Tooltip.new(root, {
+//       dy: -30,
+//       pointerOrientation: "vertical",
+//       labelText: "{valueY}"
+//     })
+//   })
+// );
 
-// heatrule
-series.set("heatRules", [
-  {
-    dataField: "valueY",
-    min: am5.color(0xe5dc36),
-    max: am5.color(0x5faa46),
-    target: series.columns.template,
-    key: "fill"
-  },
-  {
-    dataField: "valueY",
-    min: am5.color(0xe5dc36),
-    max: am5.color(0x5faa46),
-    target: circleTemplate,
-    key: "fill"
-  }
-]);
+// series.columns.template.setAll({
+//   strokeOpacity: 0,
+//   cornerRadiusBR: 10,
+//   cornerRadiusTR: 10,
+//   cornerRadiusBL: 10,
+//   cornerRadiusTL: 10,
+//   maxWidth: 50,
+//   fillOpacity: 0.8
+// });
 
-series.data.setAll(data);
-xAxis.data.setAll(data);
+// var currentlyHovered;
 
-var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {}));
-cursor.lineX.set("visible", false);
-cursor.lineY.set("visible", false);
+// series.columns.template.events.on("pointerover", function (e) {
+//   handleHover(e.target.dataItem);
+// });
 
-cursor.events.on("cursormoved", function () {
-  var dataItem = series.get("tooltip").dataItem;
-  if (dataItem) {
-    handleHover(dataItem);
-  } else {
-    handleOut();
-  }
-});
+// series.columns.template.events.on("pointerout", function (e) {
+//   handleOut();
+// });
 
-// Make stuff animate on load
-// https://www.amcharts.com/docs/v5/concepts/animations/
-series.appear();
-chart.appear(1000, 100);
+// function handleHover(dataItem) {
+//   if (dataItem && currentlyHovered != dataItem) {
+//     handleOut();
+//     currentlyHovered = dataItem;
+//     var bullet = dataItem.bullets[0];
+//     bullet.animate({
+//       key: "locationY",
+//       to: 1,
+//       duration: 600,
+//       easing: am5.ease.out(am5.ease.cubic)
+//     });
+//   }
+// }
 
-}); // end am5.ready()
+// function handleOut() {
+//   if (currentlyHovered) {
+//     var bullet = currentlyHovered.bullets[0];
+//     bullet.animate({
+//       key: "locationY",
+//       to: 0,
+//       duration: 600,
+//       easing: am5.ease.out(am5.ease.cubic)
+//     });
+//   }
+// }
+
+// var circleTemplate = am5.Template.new({});
+
+// series.bullets.push(function (root, series, dataItem) {
+//   var bulletContainer = am5.Container.new(root, {});
+//   var circle = bulletContainer.children.push(
+//     am5.Circle.new(
+//       root,
+//       {
+//         radius: 34
+//       },
+//       circleTemplate
+//     )
+//   );
+
+//   var maskCircle = bulletContainer.children.push(
+//     am5.Circle.new(root, { radius: 27 })
+//   );
+
+//   // only containers can be masked, so we add image to another container
+//   var imageContainer = bulletContainer.children.push(
+//     am5.Container.new(root, {
+//       mask: maskCircle
+//     })
+//   );
+
+//   var image = imageContainer.children.push(
+//     am5.Picture.new(root, {
+//       templateField: "pictureSettings",
+//       centerX: am5.p50,
+//       centerY: am5.p50,
+//       width: 60,
+//       height: 60
+//     })
+//   );
+
+//   return am5.Bullet.new(root, {
+//     locationY: 0,
+//     sprite: bulletContainer
+//   });
+// });
+
+// // heatrule
+// series.set("heatRules", [
+//   {
+//     dataField: "valueY",
+//     min: am5.color(0xe5dc36),
+//     max: am5.color(0x5faa46),
+//     target: series.columns.template,
+//     key: "fill"
+//   },
+//   {
+//     dataField: "valueY",
+//     min: am5.color(0xe5dc36),
+//     max: am5.color(0x5faa46),
+//     target: circleTemplate,
+//     key: "fill"
+//   }
+// ]);
+
+// series.data.setAll(data);
+// xAxis.data.setAll(data);
+
+// var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {}));
+// cursor.lineX.set("visible", false);
+// cursor.lineY.set("visible", false);
+
+// cursor.events.on("cursormoved", function () {
+//   var dataItem = series.get("tooltip").dataItem;
+//   if (dataItem) {
+//     handleHover(dataItem);
+//   } else {
+//     handleOut();
+//   }
+// });
+
+// // Make stuff animate on load
+// // https://www.amcharts.com/docs/v5/concepts/animations/
+// series.appear();
+// chart.appear(1000, 100);
+
+// }); // end am5.ready()
 
 
 /************클릭 이미지 슬라이더 기능************/
