0bf1d923f9f6e341d0db4ef17367852f6522bce8
yujin-kwak <yujin02371@gmail.com>
2023-01-04 01:30:35 +0900

    refactor: 반복 ui 자바스크립트로 구현

--- app.js
+++ app.js
@@ -29,11 +29,11 @@ const data = {
       'rgba(54, 162, 235, 0.2)'
     ],
     hoverBackgroundColor:
-    ['rgba(255, 99, 132, 0.4)',
-      'rgba(255, 159, 64, 0.4)',
-      'rgba(255, 205, 86, 0.4)',
-      'rgba(75, 192, 192, 0.4)',
-      'rgba(54, 162, 235, 0.4)'
+    ['rgba(255, 99, 132, 0.5)',
+      'rgba(255, 159, 64, 0.5)',
+      'rgba(255, 205, 86, 0.5)',
+      'rgba(75, 192, 192, 0.5)',
+      'rgba(54, 162, 235, 0.5)'
     ],
     borderColor: [
       'rgb(255, 99, 132)',
@@ -124,16 +124,31 @@ document.querySelector('.left_arrow').onclick = function () {
   previous_slide.classList.add('active');
 }
 
-/************ 프로필 js *********/
+/************ 반복 UI 처리 ************/
+const profile_data = document.querySelector('#profile_data');
+const album_data = document.querySelector('#album_list_info');
+const drama_data = document.querySelector('#drama_data');
+const career_data = document.querySelector('#career_data > ul');
+const award_data = document.querySelector('#award_data > ul');
 
-// const main_profile_data_dt = document.querySelectorAll('#profile_data_dt');
-// const main_profile_data_dd = document.querySelectorAll('#profile_data_dd');
+(async () => {
+    const res = await fetch("profile_text.json");
+    const content = await res.json();
 
-// const profile_data_dt = ["이름", "출생", "나이", "소속사", "데뷔"]
-// const profile_data_dd = [": 이지은, IU", ": 1993.05.16", ": 30세, 만29세",": EDAM 엔터테인먼트",": 2008년 미니 앨범 [Lost and Found]"]
+    const profile_data_list = content.profile_text.map(({dt, dd}) => `<div class='info_group'><dt>${dt}</dt><dd>${dd}</dd></div>`).join('');
+    profile_data.innerHTML=profile_data_list;
+    
+    //const album_data_list_dl = content.album_content_dl.map(([{dt, dd}]) => `<div class='info_group'><dt>${dt}</dt><dd>${dd}</dd></div>`).join('');
+    const album_data_list = content.album_content.map(({img, strong, alt, dt_one, dd_one, dt_two, dd_two, dt_three, dd_three}) => `<section class='album_box'><section class="album_image"><img src='${img}' alt='${alt}'></section><section class="album_content"><div class="album_explain"><strong>${strong}</strong><dl class='info'><div class='info_group'><dt>${dt_one}</dt><dd>${dd_one}</dd></div><div class='info_group'><dt>${dt_two}</dt><dd>${dd_two}</dd></div><div class='info_group'><dt>${dt_three}</dt><dd>${dd_three}</dd></div></dl></div></section></section>`).join('');
+    album_data.innerHTML=album_data_list;
 
-// for(let i=0;i<5;i++){
-//   main_profile_data_dt[i].innerHTML = profile_data_dt[i];
-//   main_profile_data_dd[i].innerHTML = profile_data_dd[i];
-// }
+    const drama_data_list = content.drama_text.map(({h3, span, p}) => `<div class='drama_explain'><h3>${h3}<span class='role'>${span}</span></h3><p>${p}</p></div>`).join('');
+    drama_data.innerHTML=drama_data_list;
+
+    const career_data_list = content.career_text.map(({span, li}) => `<li><span class="info_date">${span}</span>${li}</li>`).join('');
+    career_data.innerHTML=career_data_list;
+
+    const award_data_list = content.award_text.map(({span, li}) => `<li><span class="info_date">${span}</span>${li}</li>`).join('');
+    award_data.innerHTML=award_data_list;
+})();
 
